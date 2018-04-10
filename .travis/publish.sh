d18e9464e4bc59a825bd80469fcb1a06af1dbad5
devbhuwan <bhuwan.upadhyay49@gmail.comm>
2018-04-10 23:10:04 +0400

    Release

--- .travis/publish.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if [ $TRAVIS_BRANCH = 'master' ] && [ $TRAVIS_PULL_REQUEST == 'false' ]; then
-
-    mvn deploy --settings $TRAVIS_DIR/settings.xml -Drevision=$BUILD_REVISION -DperformRelease=true -DskipTests=true
-    exit $?
-
-fi
