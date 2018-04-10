d7d187acb17a8b1ca4c0914613ee0f5a7e7bc952
devbhuwan <bhuwan.upadhyay49@gmail.comm>
2018-04-10 22:24:39 +0400

    Release

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 if [ $TRAVIS_BRANCH = 'master' ] && [ $TRAVIS_PULL_REQUEST == 'false' ]; then
 
-    openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $TRAVIS_DIR/pubring.gpg.enc -out $TRAVIS_DIR/pubring.gpg -d
-    openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $TRAVIS_DIR/secring.gpg.enc -out $TRAVIS_DIR/secring.gpg -d
-
     mvn deploy --settings $TRAVIS_DIR/settings.xml \
      -Drevision=$BUILD_REVISION \
      -DperformRelease=true -DskipTests=true \
