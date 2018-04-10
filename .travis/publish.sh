72e71fb1371dcc3eccd64abe910123f6f2cbff41
devbhuwan <bhuwan.upadhyay49@gmail.comm>
2018-04-10 22:49:03 +0400

    Release

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ $TRAVIS_BRANCH = 'master' ] && [ $TRAVIS_PULL_REQUEST == 'false' ]; then
 
-    mvn deploy --settings $TRAVIS_DIR/settings.xml -Drevision=$BUILD_REVISION -DperformRelease=true -DskipTests=true -Dgpg.executable=gpg  -Dgpg.keyname=$GPG_KEYNAME -Dgpg.passphrase=$GPG_PASSPHRASE -Dgpg.defaultKeyring=false -Dgpg.publicKeyring=$TRAVIS_DIR/pubring.kbx -Dgpg.secretKeyring=$TRAVIS_DIR/trustdb.gpg
+    mvn deploy --settings $TRAVIS_DIR/settings.xml -Drevision=$BUILD_REVISION -DperformRelease=true -DskipTests=true
     exit $?
 
 fi
